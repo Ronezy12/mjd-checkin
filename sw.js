7dbf04229872e2637d95595ce8107e1db7a4fa2a
Ronezy12 <aharonelbaz6@gmail.com>
2026-02-18 19:10:33 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "cteen-checkin-v3"; // ✅ change la version à chaque update
+const CACHE_NAME = "cteen-checkin-v6"; // ✅ change la version à chaque update
 const ASSETS = [
   "/mjd-checkin/",
   "/mjd-checkin/login.html",
@@ -19,11 +19,11 @@ self.addEventListener("install", (event) => {
 });
 
 self.addEventListener("activate", (event) => {
-  event.waitUntil((async () => {
-    const keys = await caches.keys();
-    await Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)));
-    await self.clients.claim();
-  })());
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+     ).then(()=> self.clients.claim())
+  );
 });
 
 self.addEventListener("fetch", (event) => {
